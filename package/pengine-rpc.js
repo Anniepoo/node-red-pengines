8319dc325c7dabbf08c9847da4346b7045ecd867
Anne Ogborn <annie@thelginworks.com>
2018-09-20 17:36:49 -0700

    now loading properly, but global.get not a function

--- package/pengine-rpc.js
+++ package/pengine-rpc.js
@@ -1,32 +1,32 @@
-module.exports = function(RED) {
-    function PengineRPCNode(config) {
-        RED.nodes.createNode(this,config);
-        var node = this;
-        node.on('input', function(msg) {
-		var pengines = global.get('penginesModule')
-		var serverurl = context.get('server');
-		var chunk = int(context.get('chunk');
-                var code = context.get('sourceText');
-                var template = context.get('template');
-		peng = pengines({
-			server: serverurl,
-			chunk: chunk,
-			sourceText: code,
-			ask: msg.payload,
-			template: template
-			} )
-		    .on('success',function(result){
-			    node.warn(result);
-			    node.warn(result.data[0])
-				for(var resultData in result.data)
-				    node.send(result.data[resultData]);
-			    })
-		    .on('error', function(result){
-			 node.warn(result);
-			 node.error("pengine error ", msg);
-			 })
-		
-        });
-    }
-    RED.nodes.registerType("pengine-rpc",PengineRPCNode);
-}
+	module.exports = function(RED) {
+	    function PengineRPCNode(config) {
+	        RED.nodes.createNode(this,config);
+	        var node = this;
+	        node.on('input', function(msg) {
+			var pengines = global.get('penginesModule');
+			var serverurl = context.get('server');
+			var chunk = int(context.get('chunk'));
+          var code = context.get('sourceText');
+          var template = context.get('template');
+			pengines({
+				server: serverurl,
+				chunk: chunk,
+				sourceText: code,
+				ask: msg.payload,
+				template: template
+				} )
+			    .on('success',function(result){
+					node.warn(result);
+					node.warn(result.data[0]);
+					for(var resultData in result.data) {
+					    node.send(result.data[resultData]);
+					}
+				    })
+			    .on('error', function(result){
+					 node.warn(result);
+					 node.error("pengine error ", msg);
+				 });
+	        });
+	    }
+	    RED.nodes.registerType("pengine-rpc",PengineRPCNode);
+	}
